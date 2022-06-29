6d499c6f5330504842fbdb16043c1fe27703182b
shyunju7 <shyunju7@naver.com>
2022-06-29 19:26:10 +0900

    Feat apply codes.controller & service to app.module

--- shopping-list-server/src/app.module.ts
+++ shopping-list-server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CodesController } from './codes/codes.controller';
+import { CodesService } from './codes/codes.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CodesController],
+  providers: [AppService, CodesService],
 })
 export class AppModule {}
