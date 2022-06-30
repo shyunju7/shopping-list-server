7bfa28515df49ef07d18dca7ebd2fa6f8e065844
shyunju7 <shyunju7@naver.com>
2022-06-30 21:01:40 +0900

    Feat apply constructor & getUserInfo

--- shopping-list-server/src/user/user.controller.ts
+++ shopping-list-server/src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get(':code')
-  getUserInfo(code: string) {
-    return 'user all info';
+  getUserInfo(@Param('code') userCode: string) {
+    return this.userService.getUserInfo(userCode);
   }
 }
