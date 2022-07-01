9bbf2e864218138e0ec763a935c755ec4d8a2fb2
shyunju7 <shyunju7@naver.com>
2022-07-01 22:38:08 +0900

    Feat apply UserService & UserController

--- shopping-list-server/src/app.module.ts
+++ shopping-list-server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CodesController } from './codes/codes.controller';
 import { CodesService } from './codes/codes.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CodesController],
-  providers: [AppService, CodesService],
+  controllers: [AppController, CodesController, UserController],
+  providers: [AppService, CodesService, UserService],
 })
 export class AppModule {}
